2af5b0271a7003bacfc4e092c679b8375fd07655
jdavidrcamacho <joao.camacho@astro.up.pt>
2020-01-29 11:21:12 +0000

    searches for all and individual

--- starsearch/archiveSearch.py
+++ starsearch/archiveSearch.py
@@ -20,13 +20,13 @@ class ESOquery(object):
     def __init__(self, user):
         self.user = str(user)
         self.eso = Eso()
-        self.eso.login(self.user)
+        self.eso.login(self.user) #login in eso 
         self.eso.ROW_LIMIT = -1 #unlimited number of search results
         
         
-    def searchDates(self, star):
+    def searchReleaseDate(self, star):
         """
-        Checks the day the data was released 
+        Checks the date the data was released 
         
         Parameters
         ----------
@@ -66,9 +66,9 @@ class ESOquery(object):
         return instrumentDict
     
     
-    def _searchAndDownload(self, star, instrument, downloadPath, date):
+    def _searchAndDownload(self, star, instrument, downloadPath, date, calib):
         """
-        Download ESO data
+        Download ESO spectra of a given star; to be used is getStarData()
         
         Parameters
         ----------
@@ -78,23 +78,29 @@ class ESOquery(object):
             Instrument we are searching the darta
         date: str
             Download only the data past a certain date
+        calib : str
+            Retrieve associated calibration files: 'none' (default), 'raw' for
+            raw calibrations, or 'processed' for processed calibrations.
             
         Returns
         -------
+        
         """
-        print('Downloading {0} data\n'.format(star))
+        print('Downloading {0} data\n'.format(instrument))
         table = self.eso.query_main(column_filters = {'instrument': instrument, 
                                                       'target': star})
         if downloadPath:
             self.eso.retrieve_data(table['Dataset ID'], 
-                                   destination = downloadPath)
+                                   destination = downloadPath,
+                                   with_calib = calib)
         else:
-            self.eso.retrieve_data(table['Dataset ID'])
+            self.eso.retrieve_data(table['Dataset ID'], with_calib = calib)
+        return 0
         
-    
-    def getStar(self, star, downloadPath = None , date = None):
+        
+    def getALLdata(self, star, downloadPath = None , date = None, calib = 'none'):
         """
-        Download ESO data
+        Download ESO spectra of a given star
         
         Parameters
         ----------
@@ -104,26 +110,153 @@ class ESOquery(object):
             Adress where to download data
         date: str
             Download only the data past a certain date
+        calib : str
+            Retrieve associated calibration files: 'None' (default), 'raw' for
+            raw calibrations, or 'processed' for processed calibrations.
             
         Returns
         -------
         
         """
-        checkInstruments = self.searchStarInstrument(star)
+        checkInstruments = self.searchInstrument(star)
         esoInst = np.array(['FEROS', 'UVES', 'HARPS', 'ESPRESSO'])
-        
         for i, j in enumerate(esoInst):
-            print('\n*** Searching for {0} results ***'.format(j))
+            print('\n*** Searching for {0} results ***\n'.format(j))
             if j in checkInstruments:
-                self._searchAndDownload(j, star, downloadPath, date)
-#                print('Downloading {0} data\n'.format(j))
-#                table = self.eso.query_main(column_filters = {'instrument': j, 
-#                                                              'target': star})
-#                if downloadPath:
-#                    self.eso.retrieve_data(table['Dataset ID'], 
-#                                           destination = downloadPath)
-#                else:
-#                    self.eso.retrieve_data(table['Dataset ID'])
+                self._searchAndDownload(star, j, downloadPath, date, calib)
             else:
                 print('No {0} data\n'.format(j))
+        print('\n*** Done ***\n')
+        return 0
+    
+    
+    def getFEROSdata(self, star, downloadPath = None , date = None, 
+                     calib = 'none'):
+        """
+        Download FEROS spectra of a given star
+        
+        Parameters
+        ----------
+        star: str
+            Name of the star
+        downloadPatch: str
+            Adress where to download data
+        date: str
+            Download only the data past a certain date
+        calib : str
+            Retrieve associated calibration files: 'None' (default), 'raw' for
+            raw calibrations, or 'processed' for processed calibrations.
+            
+        Returns
+        -------
+        
+        """
+        checkInstruments = self.searchInstrument(star)
+        esoInst = np.array(['FEROS'])
+        for i, j in enumerate(esoInst):
+            print('\n*** Searching for {0} results ***\n'.format(j))
+            if j in checkInstruments:
+                self._searchAndDownload(star, j, downloadPath, date, calib)
+            else:
+                print('No {0} data\n'.format(j))
+        print('\n*** Done ***\n')
+        return 0
+    
+    
+    def getUVESdata(self, star, downloadPath = None , date = None, 
+                    calib = 'none'):
+        """
+        Download UVES spectra of a given star
+        
+        Parameters
+        ----------
+        star: str
+            Name of the star
+        downloadPatch: str
+            Adress where to download data
+        date: str
+            Download only the data past a certain date
+        calib : str
+            Retrieve associated calibration files: 'None' (default), 'raw' for
+            raw calibrations, or 'processed' for processed calibrations.
+            
+        Returns
+        -------
+        
+        """
+        checkInstruments = self.searchInstrument(star)
+        esoInst = np.array(['UVES'])
+        for i, j in enumerate(esoInst):
+            print('\n*** Searching for {0} results ***\n'.format(j))
+            if j in checkInstruments:
+                self._searchAndDownload(star, j, downloadPath, date, calib)
+            else:
+                print('No {0} data\n'.format(j))
+        print('\n*** Done ***\n')
+        return 0
+    
+    
+    def getHARPSdata(self, star, downloadPath = None , date = None, 
+                    calib = 'none'):
+        """
+        Download HARPS spectra of a given star
+        
+        Parameters
+        ----------
+        star: str
+            Name of the star
+        downloadPatch: str
+            Adress where to download data
+        date: str
+            Download only the data past a certain date
+        calib : str
+            Retrieve associated calibration files: 'None' (default), 'raw' for
+            raw calibrations, or 'processed' for processed calibrations.
+            
+        Returns
+        -------
+        
+        """
+        checkInstruments = self.searchInstrument(star)
+        esoInst = np.array(['HARPS'])
+        for i, j in enumerate(esoInst):
+            print('\n*** Searching for {0} results ***\n'.format(j))
+            if j in checkInstruments:
+                self._searchAndDownload(star, j, downloadPath, date, calib)
+            else:
+                print('No {0} data\n'.format(j))
+        print('\n*** Done ***\n')
+        return 0
+    
+    
+    def getESPRESSOdata(self, star, downloadPath = None , date = None, 
+                        calib = 'none'):
+        """
+        Download ESPRESSO spectra of a given star
+        
+        Parameters
+        ----------
+        star: str
+            Name of the star
+        downloadPatch: str
+            Adress where to download data
+        date: str
+            Download only the data past a certain date
+        calib : str
+            Retrieve associated calibration files: 'None' (default), 'raw' for
+            raw calibrations, or 'processed' for processed calibrations.
+            
+        Returns
+        -------
+        
+        """
+        checkInstruments = self.searchInstrument(star)
+        esoInst = np.array(['ESPRESSO'])
+        for i, j in enumerate(esoInst):
+            print('\n*** Searching for {0} results ***\n'.format(j))
+            if j in checkInstruments:
+                self._searchAndDownload(star, j, downloadPath, date, calib)
+            else:
+                print('No {0} data\n'.format(j))
+        print('\n*** Done ***\n')
         return 0
