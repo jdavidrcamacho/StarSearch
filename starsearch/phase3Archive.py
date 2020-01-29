f70a1dc08554dfd0c477d6020e1eca383bfbeb89
jdavidrcamacho <joao.camacho@astro.up.pt>
2020-01-29 20:15:50 +0000

    working on phase3

--- starsearch/phase3Archive.py
+++ starsearch/phase3Archive.py
@@ -23,8 +23,38 @@ class ESOquery(object):
         self.user = str(user) #user name 
         self.eso = Eso()
         self.eso.login(self.user) #login to eso archive
-        self.eso.ROW_LIMIT = -1 #unlimited number of search results
-        self.surveys = self.eso.list_surveys() #list of surveys
+        self.eso.ROW_LIMIT = -1 #unlimited number of search results = -1
+        self.surveys = self.eso.list_surveys() #list of available surveys
+        self.instruments = ['FEROS', 'UVES', 'HARPS', 'ESPRESSO']
         
-    def searchSurveys(self, star):
-        return 0
+        
+    def searchInstrument(self, star):
+        """
+        Checks which instruments where used for the given star and how many
+        observations it made
+        
+        Parameters
+        ----------
+        star: str
+            Name of the star
+            
+        Returns
+        -------
+        instrumentDict: dict
+            Instruments and number of observations
+        """
+        searchResult = self.eso.query_main(column_filters={'target': star})
+        instruments = np.unique(np.array(searchResult['Instrument']), 
+                                return_counts=True)
+        instrumentDict = dict()
+        for i, j in enumerate(instruments[0]):
+            instrumentDict[j] = instruments[1][i]
+        return instrumentDict
+    
+    
+    def searchStar(self, star):
+        url = "http://archive.eso.org/wdb/wdb/adp/phase3_spectral/form"
+        result = self.eso.query_surveys(instrument = self.instruments, 
+                                        target = star) 
+        
+        return result
