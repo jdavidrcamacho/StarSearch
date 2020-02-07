0b70dcf8986815d5724573a772410e1280050b69
jdavidrcamacho <joao.camacho@astro.up.pt>
2020-02-07 11:15:46 +0000

    surveys need to be lists

--- starsearch/phase3Archive.py
+++ starsearch/phase3Archive.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import numpy as np
-from astroquery.eso import Eso
-#from astroquery.esocas import Eso
+from astroquery.esocas import Eso
 from astropy.time import Time
 from sys import maxsize
 np.set_printoptions(threshold = maxsize)
@@ -53,7 +52,7 @@ class ESOquery():
             instrumentDict[j] = instruments[1][i]
         return instrumentDict
     
-
+    
     def searchNumberByInstrument(self, star, instrument=None):
         """
         Checks how many spectra is available to downloan for a given instrument
@@ -81,12 +80,12 @@ class ESOquery():
                 number = np.sum(searchStar['Instrument'] == j)
                 instrumentDict[j] = number
             return instrumentDict
-    
-
+        
+        
     def searchStar(self, star, instrument=None):
         """
         Return phase 3 ESO query for given star and a given instrument
-
+        
         Parameters
         ----------
         star: str
@@ -101,13 +100,13 @@ class ESOquery():
         """
         if instrument:
             search = self.eso.query_surveys(surveys = instrument, 
-                                            target = star) 
+                                            target = star)
         else:
-            search = self.eso.query_surveys(surveys = self.instruments, 
-                                            target = star) 
+            search = self.eso.query_surveys(surveys = list(self.instruments), 
+                                            target = star)
         return search
-
-
+    
+    
     def searchObservationDate(self, star, instrument=None):
         """
         Searches for the modified Julian Date (JD - 2400000.5) of the 
@@ -133,8 +132,8 @@ class ESOquery():
                                             target = star) 
         result = Time(search['Date Obs'], format='isot', scale='utc').mjd
         return result
-
-
+    
+    
     def searchSNR(self, star, instrument=None):
         """
         Return the signal-to-noise ration on the available spectra from a given
@@ -146,7 +145,7 @@ class ESOquery():
             Name of the star
         instrument: str
             Name of the instrument, None for default instruments
-
+            
         Returns
         -------
         SNR: array
@@ -160,8 +159,8 @@ class ESOquery():
                                             target = star) 
         SNR = np.array(search['SNR (spectra)'])
         return SNR
-
-
+    
+    
     def _searchAndDownload(self, star, instrument=None, 
                             downloadPath=None, date=None):
         starARCFILE = np.array(self.searchStar(star, instrument)['ARCFILE'])
@@ -170,8 +169,36 @@ class ESOquery():
                                    destination = downloadPath)
         else:
             self.eso.retrieve_data(datasets = starARCFILE)
-
-
+            
+            
+    def _getData(self, star, instrument, downloadPath = None , date = None):
+        """
+        Downloads spectra from a given instrument of a given star 
+        
+        Parameters
+        ----------
+        star: str
+            Name of the star
+        instrument: str
+            Name of the instrument
+        downloadPath: str
+            Adress where to download data
+        date: str
+            Download only the data younger than a certain date
+            
+        Returns
+        ------
+        """
+        checkInstruments = self.searchInstruments(star)
+        for _, j in enumerate(np.array([instrument])):
+            print('\n*** Searching for {0} results ***\n'.format(j))
+            if j in checkInstruments:
+                self._searchAndDownload(star, j, downloadPath, date)
+            else:
+                print('No {0} data\n'.format(j))
+        print('\n*** Done ***\n')
+        
+        
     def getALLdata(self, star, downloadPath = None , date = None):
         """
         Download ESO spectra of a given star
@@ -197,7 +224,8 @@ class ESOquery():
                 print('No {0} data\n'.format(j))
         print('\n*** Done ***\n')
 
-
+        
+        
     def getFEROSdata(self, star, downloadPath = None , date = None):
         """
         Download FEROS spectra of a given star
@@ -222,8 +250,8 @@ class ESOquery():
             else:
                 print('No {0} data\n'.format(j))
         print('\n*** Done ***\n')
-
-
+        
+        
     def getUVESdata(self, star, downloadPath = None , date = None):
         """
         Download UVES spectra of a given star
@@ -248,8 +276,8 @@ class ESOquery():
             else:
                 print('No {0} data\n'.format(j))
         print('\n*** Done ***\n')
-
-
+        
+        
     def getHARPSdata(self, star, downloadPath = None , date = None):
         """
         Download HARPS spectra of a given star
@@ -274,3 +302,30 @@ class ESOquery():
             else:
                 print('No {0} data\n'.format(j))
         print('\n*** Done ***\n')
+        
+        
+    def getESPRESSOdata(self, star, downloadPath = None , date = None):
+        """
+        Download ESPRESSO spectra of a given star
+        
+        Parameters
+        ----------
+        star: str
+            Name of the star
+        downloadPath: str
+            Adress where to download data
+        date: str
+            Download only the data younger than a certain date
+            
+        Returns
+        ------
+        """
+        checkInstruments = self.searchInstruments(star)
+        for _, j in enumerate(np.array(['ESPRESSO'])):
+            print('\n*** Searching for {0} results ***\n'.format(j))
+            if j in checkInstruments:
+                self._searchAndDownload(star, j, downloadPath, date)
+            else:
+                print('No {0} data\n'.format(j))
+        print('\n*** Done ***\n')
+        
